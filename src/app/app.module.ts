825d22abb4e6b9c6e4229ebadbafcb5628252184
Lucas Franco Barbosa Navarro <lucasfbnavarro@gmail.com>
2023-06-08 19:43:57 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,16 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { ProdutoComponent } from './produto/produto.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PaginaUsuarioComponent } from './pagina-usuario/pagina-usuario.component';
+import { CadastrarProdutoComponent } from './cadastrar-produto/cadastrar-produto.component';
+import { AuthGuard } from './aut-guard.service';
+import { FavoritosComponent } from './favoritos/favoritos.component';
+import { ListaEditarProdutosComponent } from './lista-editar-produtos/lista-editar-produtos.component';
+import { EditarProdutoComponent } from './editar-produto/editar-produto.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -19,9 +29,17 @@ import { ProdutoComponent } from './produto/produto.component';
     FooterComponent,
     LoginComponent,
     ProdutoComponent,
+    PaginaUsuarioComponent,
+    CadastrarProdutoComponent,
+    FavoritosComponent,
+    ListaEditarProdutosComponent,
+    EditarProdutoComponent,
+    EditarUsuarioComponent,
+    PesquisaComponent,
+    LoadingComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/pagina-usuario/pagina-usuario.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-pagina-usuario',
+  templateUrl: './pagina-usuario.component.html',
+  styleUrls: ['./pagina-usuario.component.css'],
+})
+export class PaginaUsuarioComponent implements OnInit {
+  produtosCriados: any;
+  email: any;
+  dadosUsuarios: any;
+
+  constructor(private produtos: ProdutosService) {}
+
+  ngOnInit(): void {
+    firebase.auth().onAuthStateChanged((user: any) => {
+      if (user) {
+        this.email = user.email;
+        console.log(user);
+        this.produtos
+          .consultarProdutosPorusuario(this.email)
+          .then((produtos) => {
+            this.produtosCriados = produtos;
+            console.log(this.produtosCriados, produtos);
+          })
+          .catch((error) => console.log(error));
+        this.produtos.acessarDadosUsuario(this.email).then((dadosUsuarios) => {
+          this.dadosUsuarios = dadosUsuarios;
+          console.log(dadosUsuarios);
+        });
+      }
+    });
+  }
+}
